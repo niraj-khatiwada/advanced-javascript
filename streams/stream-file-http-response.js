19ece2ce99389101a6ed1cc2b5fff5185767089f
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-10-11 13:51:44 +0545

    stream large file to browser and download it

--- streams/stream-file-http-response.js
+++ streams/stream-file-http-response.js
@@ -7,8 +7,13 @@ const largeFile = fs.createReadStream(
 
 // Streaming files in Node.js
 const server = http.createServer(function (req, res) {
+  res.setHeader('Content-Length', 2.2 * 1e9) // Total size of file.
+  res.setHeader(
+    'Content-Disposition',
+    "attachment; filename=big-file.txt; modification-date='Wed, 12 Feb 1997 16:29:51 -0500"
+  )
   largeFile.on('data', function (chunk) {
-    res.write(chunk)
+    res.write(chunk, 'utf-8')
   })
 
   return
